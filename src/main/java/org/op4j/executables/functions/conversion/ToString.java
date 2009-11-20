b1010ea81dbb9e95a499eae335bb6a027da1d00d
Soraya Sánchez <soraya.schz@gmail.com>
2009-11-20 14:18:47 +0000

    Conversion from calendar added
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@134 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/executables/functions/conversion/ToString.java
+++ src/main/java/org/op4j/executables/functions/conversion/ToString.java
@@ -25,23 +25,27 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import org.apache.commons.lang.LocaleUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
 import org.javaruntype.type.Type;
 import org.javaruntype.type.Types;
 import org.op4j.executables.functions.IFunc;
+import org.op4j.executables.functions.conversion.ToString.FromCalendar.DateStyle;
+import org.op4j.executables.functions.conversion.ToString.FromCalendar.TimeStyle;
 
 /**
  * 
  * @since 1.0
  * 
- * @author Daniel Fern&aacute;ndez
+ * @author Soraya S&aacute;nchez
  *
  */
 public final class ToString {
 
 	private static final FromObject FROM_OBJECT = new FromObject();
 
+	//TODO Add conversions for java.sql.Date, java.util.Date, numbers
 	
 	private ToString() {
 		super();
@@ -52,16 +56,28 @@ public final class ToString {
 		return FROM_OBJECT;
 	}
 	
-	public static final FromCalendar fromCalendar(String pattern) {
+	public static final FromCalendar fromCalendar(final String pattern) {
 		return new FromCalendar(pattern);
 	}
 	
-	public static final FromCalendar fromCalendar(Locale locale) {
-		return new FromCalendar(locale);
+	public static final FromCalendar fromCalendar(final String pattern, final Locale locale) {
+		return new FromCalendar(pattern, locale);
 	}
 	
-	public static final FromCalendar fromCalendar(Locale locale, String pattern) {
-		return new FromCalendar(locale, pattern);
+	public static final FromCalendar fromCalendar(final String pattern, final String locale) {
+		return new FromCalendar(pattern, locale);
+	}
+	
+	public static final FromCalendar fromCalendar(final DateStyle dateStyle, final TimeStyle timeStyle) {
+		return new FromCalendar(dateStyle, timeStyle);
+	}
+	
+	public static final FromCalendar fromCalendar(final DateStyle dateStyle, final TimeStyle timeStyle, final Locale locale) {
+		return new FromCalendar(dateStyle, timeStyle, locale);
+	}
+	
+	public static final FromCalendar fromCalendar(final DateStyle dateStyle, final TimeStyle timeStyle, final String locale) {
+		return new FromCalendar(dateStyle, timeStyle, locale);
 	}
 	
 	
@@ -83,6 +99,8 @@ public final class ToString {
 	
 	public static final class FromCalendar implements IFunc<String, Calendar> {
 
+		private DateStyle dateStyle = null;	
+		private TimeStyle timeStyle = null;	
 		private Locale locale = null;
 		private String pattern = null;
 		
@@ -91,46 +109,140 @@ public final class ToString {
 			this.pattern = pattern;
 		}
 		
-		public FromCalendar(Locale locale) {
+		public FromCalendar(String pattern, Locale locale) {
 			super();
-			this.locale = locale;
-		}
-		
-		public FromCalendar(Locale locale, String pattern) {
-			super();
-			this.locale = locale;
 			this.pattern = pattern;
+			this.locale = locale;			
 		}
 		
+		public FromCalendar(String pattern, String locale) {
+			super();
+			this.pattern = pattern;
+			this.locale = LocaleUtils.toLocale(locale);
+		}
+				
+		public FromCalendar(DateStyle dateStyle, TimeStyle timeStyle) {
+			super();
+			this.dateStyle = dateStyle;
+			this.timeStyle = timeStyle;
+		}
+		
+		public FromCalendar(DateStyle dateStyle, TimeStyle timeStyle, Locale locale) {
+			super();
+			this.dateStyle = dateStyle;
+			this.timeStyle = timeStyle;
+			this.locale = locale;
+		}
+		
+		public FromCalendar(DateStyle dateStyle, TimeStyle timeStyle, String locale) {
+			super();
+			this.dateStyle = dateStyle;
+			this.timeStyle = timeStyle;
+			this.locale = LocaleUtils.toLocale(locale);
+		}
+						
 		public Type<? super String> getResultType() {
 			return Types.STRING;
 		}
 
 		public String execute(final Calendar calendar) throws Exception {
-			return fromCalendar(
-                    calendar,
-                    this.locale, 
-                    this.pattern);
+			if (StringUtils.isNotEmpty(this.pattern)) {
+				return fromCalendar(calendar, this.pattern, this.locale);
+			} 
+			return fromCalendar(calendar, this.dateStyle, this.timeStyle, this.locale);						
 		}
 		
-		private String fromCalendar(final Calendar calendar, final Locale locale, final String pattern) {
+		private String fromCalendar(final Calendar calendar, final String thePattern, final Locale theLocale) {
 	    	
-	    	Validate.isTrue((locale != null) 
-	    			|| (StringUtils.isNotEmpty(pattern)),
-	    			"Either locale or pattern must be set...both are also valid");
+			Validate.isTrue((StringUtils.isNotEmpty(thePattern))
+					|| (theLocale != null && thePattern != null),
+	    			"Either pattern or locale and pattern must be set");
 	    	
-	    	DateFormat sdf = null;
-	    	if (locale == null) {
-	    		sdf = new SimpleDateFormat(pattern);
-	    	} else {
-	    		if (StringUtils.isEmpty(pattern)) {
-	    			sdf = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-	    		} else {
-	    			sdf = new SimpleDateFormat(pattern, locale);
-	    		}
-	    	}
-	    	
+	    	DateFormat sdf = new SimpleDateFormat(thePattern, 
+	    			(theLocale == null) ? Locale.getDefault() : theLocale);	    		
+	    		    	
 	    	return sdf.format(calendar.getTime());
 	    }		
+		
+		@SuppressWarnings("null")
+		private String fromCalendar(final Calendar calendar, final DateStyle theDateStyle,
+				final TimeStyle theTimeStyle, final Locale theLocale) {
+	    	
+	    	Validate.isTrue((theDateStyle != null && theTimeStyle != null)
+	    			&& (DateStyle.NONE.compareTo(theDateStyle) != 0
+	    					|| TimeStyle.NONE.compareTo(theTimeStyle) != 0),
+	    			"At least one of dateStyle and timeStyle must have a value different from NONE");
+	    	
+	    	DateFormat sdf = null;
+	    	if (DateStyle.NONE.compareTo(theDateStyle) != 0 && (TimeStyle.NONE.compareTo(theTimeStyle) != 0)) {
+	    		sdf = DateFormat.getDateTimeInstance(Integer.parseInt(theDateStyle.toString()), 
+	    				Integer.parseInt(theTimeStyle.toString()),
+	    				(theLocale == null) ? Locale.getDefault() : theLocale);
+	    	} else if (DateStyle.NONE.compareTo(theDateStyle) != 0) {
+	    		sdf = DateFormat.getDateInstance(Integer.parseInt(theDateStyle.toString()),
+	    				(theLocale == null) ? Locale.getDefault() : theLocale);
+	    	} else {
+	    		sdf = DateFormat.getTimeInstance(Integer.parseInt(theTimeStyle.toString()),
+	    				(theLocale == null) ? Locale.getDefault() : theLocale);
+	    	}	      		
+	    	
+	    	return sdf.format(calendar.getTime());
+	    }
+		
+		public static enum DateStyle {
+			SHORT {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.SHORT);
+				}				
+			},
+			MEDIUM {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.MEDIUM);
+				}				
+			},
+			LONG {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.LONG);
+				}				
+			},
+			FULL {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.FULL);
+				}				
+			},
+			NONE
+		}
+		
+		public static enum TimeStyle {
+			SHORT {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.SHORT);
+				}				
+			},
+			MEDIUM {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.MEDIUM);
+				}				
+			},
+			LONG {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.LONG);
+				}				
+			},
+			FULL {
+				@Override
+				public String toString() {
+					return String.valueOf(DateFormat.FULL);
+				}				
+			},
+			NONE
+		}		
 	}
 }
