8b9f3873e9be1a76dd0bde02df7d36df5ae2e12b
Daniel Fernández <daniel.fernandez@11thlabs.org>
2010-01-26 00:17:28 +0000

    Several changes made
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@313 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/functions/StringFuncs.java
+++ src/main/java/org/op4j/functions/StringFuncs.java
@@ -37,86 +37,86 @@ import org.javaruntype.type.Types;
  */
 public class StringFuncs {
 
-	private static EscapeCSVStringFunc ESCAPE_CSV_STRING_FUNC = new EscapeCSVStringFunc();
-	private static UnescapeCSVStringFunc UNESCAPE_CSV_STRING_FUNC = new UnescapeCSVStringFunc();
-	private static EscapeXMLStringFunc ESCAPE_XML_STRING_FUNC = new EscapeXMLStringFunc();
-	private static UnescapeXMLStringFunc UNESCAPE_XML_STRING_FUNC = new UnescapeXMLStringFunc();
-	private static EscapeHTMLStringFunc ESCAPE_HTML_STRING_FUNC = new EscapeHTMLStringFunc();
-	private static UnescapeHTMLStringFunc UNESCAPE_HTML_STRING_FUNC = new UnescapeHTMLStringFunc();
-	private static EscapeJavaScriptStringFunc ESCAPE_JAVASCRIPT_STRING_FUNC = new EscapeJavaScriptStringFunc();
-	private static UnescapeJavaScriptStringFunc UNESCAPE_JAVASCRIPT_STRING_FUNC = new UnescapeJavaScriptStringFunc();
-	private static ToUpperCaseStringFunc TO_UPPER_CASE_STRING_FUNC = new ToUpperCaseStringFunc();
-	private static ToLowerCaseStringFunc TO_LOWER_CASE_STRING_FUNC = new ToLowerCaseStringFunc();
-	private static UnCapitalizeStringFunc UN_CAPITALIZE_STRING_FUNC = new UnCapitalizeStringFunc();
-	private static CapitalizeStringFunc CAPITALIZE_STRING_FUNC = new CapitalizeStringFunc();
-	private static TrimStringFunc TRIM_STRING_FUNC = new TrimStringFunc();
-	private static StripStringFunc STRIP_STRING_FUNC = new StripStringFunc();
+	private static EscapeCSV ESCAPE_CSV_STRING_FUNC = new EscapeCSV();
+	private static UnescapeCSV UNESCAPE_CSV_STRING_FUNC = new UnescapeCSV();
+	private static EscapeXML ESCAPE_XML_STRING_FUNC = new EscapeXML();
+	private static UnescapeXML UNESCAPE_XML_STRING_FUNC = new UnescapeXML();
+	private static EscapeHTML ESCAPE_HTML_STRING_FUNC = new EscapeHTML();
+	private static UnescapeHTML UNESCAPE_HTML_STRING_FUNC = new UnescapeHTML();
+	private static EscapeJavaScript ESCAPE_JAVASCRIPT_STRING_FUNC = new EscapeJavaScript();
+	private static UnescapeJavaScript UNESCAPE_JAVASCRIPT_STRING_FUNC = new UnescapeJavaScript();
+	private static ToUpperCase TO_UPPER_CASE_STRING_FUNC = new ToUpperCase();
+	private static ToLowerCase TO_LOWER_CASE_STRING_FUNC = new ToLowerCase();
+	private static UnCapitalize UN_CAPITALIZE_STRING_FUNC = new UnCapitalize();
+	private static Capitalize CAPITALIZE_STRING_FUNC = new Capitalize();
+	private static Trim TRIM_STRING_FUNC = new Trim();
+	private static Strip STRIP_STRING_FUNC = new Strip();
 	
 	private StringFuncs() {
 		super();           
 	}
 
-	public static final EscapeCSVStringFunc escapeCSV() {
+	public static final EscapeCSV escapeCSV() {
         return ESCAPE_CSV_STRING_FUNC;
     }
-	public static final UnescapeCSVStringFunc unescapeCSV() {
+	public static final UnescapeCSV unescapeCSV() {
         return UNESCAPE_CSV_STRING_FUNC;
     }	
 
-	public static final EscapeXMLStringFunc escapeXML() {
+	public static final EscapeXML escapeXML() {
         return ESCAPE_XML_STRING_FUNC;
     }
-	public static final UnescapeXMLStringFunc unescapeXML() {
+	public static final UnescapeXML unescapeXML() {
         return UNESCAPE_XML_STRING_FUNC;
     }
 	
-	public static final EscapeHTMLStringFunc escapeHTML() {
+	public static final EscapeHTML escapeHTML() {
         return ESCAPE_HTML_STRING_FUNC;
     }
-	public static final UnescapeHTMLStringFunc unescapeHTML() {
+	public static final UnescapeHTML unescapeHTML() {
         return UNESCAPE_HTML_STRING_FUNC;
     }
 	
-	public static final EscapeJavaScriptStringFunc escapeJavaScript() {
+	public static final EscapeJavaScript escapeJavaScript() {
         return ESCAPE_JAVASCRIPT_STRING_FUNC;
     }
-	public static final UnescapeJavaScriptStringFunc unescapeJavaScript() {
+	public static final UnescapeJavaScript unescapeJavaScript() {
         return UNESCAPE_JAVASCRIPT_STRING_FUNC;
     }
 	
-	public static final ToBase64StringFunc toBase64(Charset charset) {
-        return new ToBase64StringFunc(charset);
+	public static final ToBase64 toBase64(Charset charset) {
+        return new ToBase64(charset);
     }
-	public static final FromBase64StringFunc fromBase64(Charset charset) {
-        return new FromBase64StringFunc(charset);
-    }
-	
-	public static final ToHexadecimalStringFunc toHexadecimal(Charset charset) {
-        return new ToHexadecimalStringFunc(charset);
-    }
-	public static final FromHexadecimalStringFunc fromHexadecimal(Charset charset) {
-        return new FromHexadecimalStringFunc(charset);
+	public static final FromBase64 fromBase64(Charset charset) {
+        return new FromBase64(charset);
     }
 	
-	public static final ToUpperCaseStringFunc toUpperCase() {
+	public static final ToHexadecimal toHexadecimal(Charset charset) {
+        return new ToHexadecimal(charset);
+    }
+	public static final FromHexadecimal fromHexadecimal(Charset charset) {
+        return new FromHexadecimal(charset);
+    }
+	
+	public static final ToUpperCase toUpperCase() {
         return TO_UPPER_CASE_STRING_FUNC;
     }
-	public static final ToLowerCaseStringFunc toLowerCase() {
+	public static final ToLowerCase toLowerCase() {
         return TO_LOWER_CASE_STRING_FUNC;
     }
 	
-	public static final UnCapitalizeStringFunc unCapitalize() {
+	public static final UnCapitalize unCapitalize() {
         return UN_CAPITALIZE_STRING_FUNC;
     }
-	public static final CapitalizeStringFunc capitalize() {
+	public static final Capitalize capitalize() {
         return CAPITALIZE_STRING_FUNC;
     }
 	
-	public static final TrimStringFunc trim() {
+	public static final Trim trim() {
         return TRIM_STRING_FUNC;
     }
 	
-	public static final StripStringFunc strip() {
+	public static final Strip strip() {
         return STRIP_STRING_FUNC;
     }
 	
@@ -136,9 +136,9 @@ public class StringFuncs {
 	 * The String is returned in a way it can be used to fill in a CSV column as StringEscapeUtils does
 	 *
 	 */
-	public static final class EscapeCSVStringFunc extends BaseStringFunc<String> {
+	public static final class EscapeCSV extends BaseStringFunc<String> {
 		
-		public EscapeCSVStringFunc() {
+		public EscapeCSV() {
 			super();
 		}
 
@@ -154,8 +154,8 @@ public class StringFuncs {
 	 * as StringEscapeUtils does
 	 *
 	 */
-	public static final class UnescapeCSVStringFunc extends BaseStringFunc<String> {
-		public UnescapeCSVStringFunc() {
+	public static final class UnescapeCSV extends BaseStringFunc<String> {
+		public UnescapeCSV() {
 			super();
 		}
 
@@ -169,8 +169,8 @@ public class StringFuncs {
 	 * The String is returned with the XML characters escaped as StringEscapeUtils does
 	 *
 	 */
-	public static final class EscapeXMLStringFunc extends BaseStringFunc<String> {
-		public EscapeXMLStringFunc() {
+	public static final class EscapeXML extends BaseStringFunc<String> {
+		public EscapeXML() {
 			super();
 		}
 
@@ -184,8 +184,8 @@ public class StringFuncs {
 	 * The String is returned without the XML escape characters as StringEscapeUtils does
 	 *
 	 */
-	public static final class UnescapeXMLStringFunc extends BaseStringFunc<String> {
-		public UnescapeXMLStringFunc() {
+	public static final class UnescapeXML extends BaseStringFunc<String> {
+		public UnescapeXML() {
 			super();
 		}
 
@@ -199,8 +199,8 @@ public class StringFuncs {
 	 * It escapes the given String using HTML entities (as StringEscapeUtils does)
 	 *
 	 */
-	public static final class EscapeHTMLStringFunc extends BaseStringFunc<String> {
-		public EscapeHTMLStringFunc() {
+	public static final class EscapeHTML extends BaseStringFunc<String> {
+		public EscapeHTML() {
 			super();
 		}
 
@@ -215,8 +215,8 @@ public class StringFuncs {
 	 * their unicode characters (as StringEscapeUtils does)
 	 *
 	 */
-	public static final class UnescapeHTMLStringFunc extends BaseStringFunc<String> {
-		public UnescapeHTMLStringFunc() {
+	public static final class UnescapeHTML extends BaseStringFunc<String> {
+		public UnescapeHTML() {
 			super();
 		}
 
@@ -230,8 +230,8 @@ public class StringFuncs {
 	 * It converts the given String into a JavaScript valid one (as StringEscapeUtils does)
 	 *
 	 */
-	public static final class EscapeJavaScriptStringFunc extends BaseStringFunc<String> {
-		public EscapeJavaScriptStringFunc() {
+	public static final class EscapeJavaScript extends BaseStringFunc<String> {
+		public EscapeJavaScript() {
 			super();
 		}
 
@@ -245,8 +245,8 @@ public class StringFuncs {
 	 * It unescapes the given JavaScript valid String (as StringEscapeUtils does)
 	 *
 	 */
-	public static final class UnescapeJavaScriptStringFunc extends BaseStringFunc<String> {
-		public UnescapeJavaScriptStringFunc() {
+	public static final class UnescapeJavaScript extends BaseStringFunc<String> {
+		public UnescapeJavaScript() {
 			super();
 		}
 
@@ -260,11 +260,11 @@ public class StringFuncs {
 	 * It converts the given String into a base64 encoded one
 	 *
 	 */
-	public static final class ToBase64StringFunc extends BaseStringFunc<String> {
+	public static final class ToBase64 extends BaseStringFunc<String> {
 
 		private Charset charset = null;
 
-		public ToBase64StringFunc(Charset charset) {
+		public ToBase64(Charset charset) {
 			super();
 			this.charset = charset;
 		}
@@ -281,11 +281,11 @@ public class StringFuncs {
 	 * It decodes the given base64 encoded String
 	 *
 	 */
-	public static final class FromBase64StringFunc extends BaseStringFunc<String> {
+	public static final class FromBase64 extends BaseStringFunc<String> {
 
 		private Charset charset = null;
 
-		public FromBase64StringFunc(Charset charset) {
+		public FromBase64(Charset charset) {
 			super();
 			this.charset = charset;
 		}
@@ -302,11 +302,11 @@ public class StringFuncs {
 	 * It converts the given String into its Hexadecimal representation using the specified Charset
 	 *
 	 */
-	public static final class ToHexadecimalStringFunc extends BaseStringFunc<String> {
+	public static final class ToHexadecimal extends BaseStringFunc<String> {
 
 		private Charset charset = null;
 
-		public ToHexadecimalStringFunc(Charset charset) {
+		public ToHexadecimal(Charset charset) {
 			super();
 			this.charset = charset;
 		}
@@ -328,11 +328,11 @@ public class StringFuncs {
 	 * The given String is converted from its Hexadecimal representation to a String using the specified Charset
 	 *
 	 */
-	public static final class FromHexadecimalStringFunc extends BaseStringFunc<String> {
+	public static final class FromHexadecimal extends BaseStringFunc<String> {
 
 		private Charset charset = null;
 
-		public FromHexadecimalStringFunc(Charset charset) {
+		public FromHexadecimal(Charset charset) {
 			super();
 			this.charset = charset;
 		}
@@ -358,9 +358,9 @@ public class StringFuncs {
 	 * It converts the given String to uppercase
 	 *
 	 */
-	public static final class ToUpperCaseStringFunc extends BaseStringFunc<String> {
+	public static final class ToUpperCase extends BaseStringFunc<String> {
 
-		public ToUpperCaseStringFunc() {
+		public ToUpperCase() {
 			super();			
 		}
 
@@ -374,9 +374,9 @@ public class StringFuncs {
 	 * It converts the given String to lowercase
 	 *
 	 */
-	public static final class ToLowerCaseStringFunc extends BaseStringFunc<String> {
+	public static final class ToLowerCase extends BaseStringFunc<String> {
 
-		public ToLowerCaseStringFunc() {
+		public ToLowerCase() {
 			super();			
 		}
 
@@ -390,9 +390,9 @@ public class StringFuncs {
 	 * It converts the first letter of the given String to lowercase
 	 *
 	 */
-	public static final class UnCapitalizeStringFunc extends BaseStringFunc<String> {
+	public static final class UnCapitalize extends BaseStringFunc<String> {
 
-		public UnCapitalizeStringFunc() {
+		public UnCapitalize() {
 			super();			
 		}
 
@@ -405,9 +405,9 @@ public class StringFuncs {
 	 * It converts the first letter of the given String to uppercase
 	 *
 	 */
-	public static final class CapitalizeStringFunc extends BaseStringFunc<String> {
+	public static final class Capitalize extends BaseStringFunc<String> {
 
-		public CapitalizeStringFunc() {
+		public Capitalize() {
 			super();			
 		}
 
@@ -421,9 +421,9 @@ public class StringFuncs {
 	 * Removes control characters (char <= 32) from both ends of the given String
 	 *
 	 */
-	public static final class TrimStringFunc extends BaseStringFunc<String> {
+	public static final class Trim extends BaseStringFunc<String> {
 
-		public TrimStringFunc() {
+		public Trim() {
 			super();			
 		}
 
@@ -437,9 +437,9 @@ public class StringFuncs {
 	 * Strips whitespace from both sides of the given String
 	 *
 	 */
-	public static final class StripStringFunc extends BaseStringFunc<String> {
+	public static final class Strip extends BaseStringFunc<String> {
 
-		public StripStringFunc() {
+		public Strip() {
 			super();			
 		}
 
