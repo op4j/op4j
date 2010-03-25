9090a57d581928d235e0761faa07a77dc4a7adf7
Daniel Fernández <daniel.fernandez@11thlabs.org>
2010-03-25 21:50:36 +0000

    Several changes made
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@717 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/test/java/org/op4j/RecipesTests.java
+++ src/test/java/org/op4j/RecipesTests.java
@@ -1,7 +1,9 @@
 package org.op4j;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -10,7 +12,9 @@ import java.util.Set;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
+import org.op4j.functions.FnString;
 
 public class RecipesTests extends TestCase {
 
@@ -89,7 +93,7 @@ public class RecipesTests extends TestCase {
         
         
         Map<String,String> capitals =
-            Op.onListFor("Spain", "Madrid", "United Kingdom", "London", "France", "Paris").couple().get();            Map<String,Integer> agesByName =
+            Op.onListFor("Spain", "Madrid", "United Kingdom", "London", "France", "Paris").couple().get();
                 
         
         assertEquals(result, capitals);
@@ -99,6 +103,25 @@ public class RecipesTests extends TestCase {
 
     
     
+    
+    @Test
+    public void testOP4J_005() throws Exception {
+
+        Calendar result = Calendar.getInstance();
+        result = DateUtils.truncate(result, Calendar.YEAR);
+        result.set(Calendar.YEAR, 1978);
+        result.set(Calendar.MONTH, 11);
+        result.set(Calendar.DAY_OF_MONTH, 6);
+
+        
+        String date = "06/12/1978";        
+        
+        Calendar cal =
+            Op.on(date).exec(FnString.toCalendar("dd/MM/yyyy")).get();
+        
+        assertEquals(result.getTimeInMillis(), cal.getTimeInMillis());
+        
+    }
     
     
     
