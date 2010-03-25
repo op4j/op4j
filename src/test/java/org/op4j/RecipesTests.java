7ad9e5f1d1f8688ead885a85be28665862e01c96
Daniel Fernández <daniel.fernandez@11thlabs.org>
2010-03-25 23:31:16 +0000

    Several changes made
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@718 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/test/java/org/op4j/RecipesTests.java
+++ src/test/java/org/op4j/RecipesTests.java
@@ -1,12 +1,13 @@
 package org.op4j;
 
 
-import java.text.SimpleDateFormat;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -122,6 +123,90 @@ public class RecipesTests extends TestCase {
         assertEquals(result.getTimeInMillis(), cal.getTimeInMillis());
         
     }
+    
+
+    
+    
+    @Test
+    public void testOP4J_006() throws Exception {
+
+        Calendar resCal1 = Calendar.getInstance();
+        resCal1 = DateUtils.truncate(resCal1, Calendar.YEAR);
+        resCal1.set(Calendar.YEAR, 1492);
+        resCal1.set(Calendar.MONTH, 9);
+        resCal1.set(Calendar.DAY_OF_MONTH, 12);
+
+        Calendar resCal2 = Calendar.getInstance();
+        resCal2 = DateUtils.truncate(resCal2, Calendar.YEAR);
+        resCal2.set(Calendar.YEAR, 1978);
+        resCal2.set(Calendar.MONTH, 11);
+        resCal2.set(Calendar.DAY_OF_MONTH, 6);
+
+        List<Calendar> result = Arrays.asList(new Calendar[] { resCal1, resCal2 });
+        
+        
+        List<String> dates = 
+            Arrays.asList(new String[] { "12 de octubre, 1492", "06 de diciembre, 1978" });
+
+        {
+            List<Calendar> cals =
+                Op.on(dates).map(FnString.toCalendar("dd 'de' MMMM, yyyy", "es")).get();
+            
+            for (int i = 0; i < cals.size(); i++) {
+                assertEquals(result.get(i).getTimeInMillis(), cals.get(i).getTimeInMillis());
+            }
+        }
+
+        {
+            List<Calendar> cals =
+                Op.on(dates).forEach().exec(FnString.toCalendar("dd 'de' MMMM, yyyy", "es")).get();
+            
+            for (int i = 0; i < cals.size(); i++) {
+                assertEquals(result.get(i).getTimeInMillis(), cals.get(i).getTimeInMillis());
+            }
+        }
+        
+    }
+    
+    
+    
+    @Test
+    @SuppressWarnings("boxing")
+    public void testOP4J_007() throws Exception {
+
+        final Integer[] result = new Integer[] {12, 43, 92, 34 };
+        
+
+        final Integer[] values = new Integer[] {null, 12, 43, 92, null, 34 };
+        
+        Integer[] filteredValues =
+            Op.on(values).removeAllNull().get();
+            
+        assertEquals(Arrays.asList(result), Arrays.asList(filteredValues));
+        
+
+    }
+    
+    
+    
+    
+    @Test
+    public void testOP4J_008() throws Exception {
+
+        final Double result = Double.valueOf(34.59);
+
+        final String strValue = "34,59";
+        
+        Double value =
+            Op.on(strValue).exec(FnString.toDouble("fr")).get();
+            
+        assertEquals(result, value);
+        
+
+    }
+    
+
+    
     
     
     
