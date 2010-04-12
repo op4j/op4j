bfc75a2905ca8a436d521448e2b22e53e37616fc
Soraya Sánchez <soraya.schz@gmail.com>
2010-04-12 21:33:37 +0000

    Changes in javadoc
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@960 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/functions/FnBigDecimal.java
+++ src/main/java/org/op4j/functions/FnBigDecimal.java
@@ -149,7 +149,7 @@ public final class FnBigDecimal {
      * target number, 0's will be added to the left of the integer part
      * @param minFractionDigits minimum number of fraction digits so, if not enough in the
      * target number, 0's will be added to the right of the decimal part
-     * @param maxFractionDigits
+     * @param maxFractionDigits maximum number of fraction digits
      * @param groupingUsed whether or not grouping will be used
      * @return the {@link String} representation of the input
      */
@@ -166,16 +166,16 @@ public final class FnBigDecimal {
      * It returns the {@link String} representation of the target number in the given locale. 
      * If necessary, it will add leading or trailing zeros to the string based on the given parameters. So,
      * </p>
-     * <code>toStr(Locale.ENGLISH, 3, 2, 2, true) would return 010.00 if target number is 10</code>
+     * <code>toStr(Locale.ENGLISH.toString(), 3, 2, 2, true) would return 010.00 if target number is 10</code>
      * <br>
-     * <code>toStr(Locale.ENGLISH, 2, 2, 4, true) would return 00.00 if target number is 0</code>
+     * <code>toStr(Locale.ENGLISH.toString(), 2, 2, 4, true) would return 00.00 if target number is 0</code>
      *    
      * @param locale the locale to be used
      * @param minIntegerDigits minimum number of integer digits so, if not enough in the
      * target number, 0's will be added to the left of the integer part
      * @param minFractionDigits minimum number of fraction digits so, if not enough in the
      * target number, 0's will be added to the right of the decimal part
-     * @param maxFractionDigits
+     * @param maxFractionDigits maximum number of fraction digits
      * @param groupingUsed whether or not grouping will be used
      * @return the {@link String} representation of the input
      */
@@ -186,6 +186,28 @@ public final class FnBigDecimal {
                 minFractionDigits, maxFractionDigits,
                 groupingUsed));
     }
+    
+    /**
+     * <p>
+     * It returns the {@link String} representation of the target number in the given {@link Locale}. 
+     * If necessary, it will add leading or trailing zeros to the string based on the given parameters. So,
+     * </p>
+     * <code>toStr(Locale.ENGLISH, 3, 2, 2, ',', '.', true) would return 1,000.00 if target number is 1000</code>
+     * <br>
+     * <code>toStr(Locale.ENGLISH, 2, 2, 4, ',', '.', true) would return 00.00 if target number is 0</code>
+     *        
+     * @param locale the {@link Locale} to be used
+     * @param minIntegerDigits minimum number of integer digits so, if not enough in the
+     * target number, 0's will be added to the left of the integer part
+     * @param minFractionDigits minimum number of fraction digits so, if not enough in the
+     * target number, 0's will be added to the right of the decimal part
+     * @param maxFractionDigits maximum number of fraction digits
+     * @param groupingSeparator grouping separator to be used
+     * @param decimalSeparator decimal separator to be used 
+     * @param decimalSeparatorAlwaysShown whether decimal separator should be shown 
+     * even if the decimal part is zero or not
+     * @return the {@link String} representation of the input
+     */
     public static final Function<BigDecimal,String> toStr(Locale locale, int minIntegerDigits,
             int minFractionDigits, int maxFractionDigits, 
             char groupingSeparator, char decimalSeparator, boolean decimalSeparatorAlwaysShown) {
