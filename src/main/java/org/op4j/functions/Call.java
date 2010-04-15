5bef76966a2aab7019912b139bd09f829ed60344
Daniel Fernández <daniel.fernandez@11thlabs.org>
2010-04-15 22:44:24 +0000

    Several changes made
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@998 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/functions/Call.java
+++ src/main/java/org/op4j/functions/Call.java
@@ -35,6 +35,9 @@ import org.op4j.util.VarArgsUtil;
 
 
 /**
+ * <p>
+ * Function hub class for executing methods of target objects.
+ * </p>
  * 
  * @since 1.0
  * 
@@ -49,89 +52,322 @@ public final class Call<T,R> extends Function<T,R> {
     private final Object[] parameters;
     
     
-    
+
+    /**
+     * <p>
+     * Executes a method on the target object which returns Object. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Object> methodForObject(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Object>(Types.OBJECT, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
+
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns an object of the <tt>resultType</tt>. 
+     * Parameters must match those of the method.
+     * </p>
+     *
+     * @param resultType the type of the result
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,R> methodFor(final Type<R> resultType, final String methodName, final Object... optionalParameters) {
         return new Call<Object,R>(resultType, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
 
 
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns BigInteger. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,BigInteger> methodForBigInteger(final String methodName, final Object... optionalParameters) {
         return new Call<Object,BigInteger>(Types.BIG_INTEGER, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
+
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns BigDecimal. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,BigDecimal> methodForBigDecimal(final String methodName, final Object... optionalParameters) {
         return new Call<Object,BigDecimal>(Types.BIG_DECIMAL, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
+
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns Boolean. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Boolean> methodForBoolean(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Boolean>(Types.BOOLEAN, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Byte. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Byte> methodForByte(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Byte>(Types.BYTE, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Character. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Character> methodForCharacter(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Character>(Types.CHARACTER, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+
+    /**
+     * <p>
+     * Executes a method on the target object which returns Calendar. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Calendar> methodForCalendar(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Calendar>(Types.CALENDAR, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Date. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Date> methodForDate(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Date>(Types.DATE, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Double. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Double> methodForDouble(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Double>(Types.DOUBLE, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Float. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Float> methodForFloat(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Float>(Types.FLOAT, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Integer. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Integer> methodForInteger(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Integer>(Types.INTEGER, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Long. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Long> methodForLong(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Long>(Types.LONG, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns Short. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Short> methodForShort(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Short>(Types.SHORT, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns String. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,String> methodForString(final String methodName, final Object... optionalParameters) {
         return new Call<Object,String>(Types.STRING, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns List&lt;String&gt;. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,List<String>> methodForListOfString(final String methodName, final Object... optionalParameters) {
         return new Call<Object,List<String>>(Types.LIST_OF_STRING, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
+
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns Set&lt;String&gt;. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,Set<String>> methodForSetOfString(final String methodName, final Object... optionalParameters) {
         return new Call<Object,Set<String>>(Types.SET_OF_STRING, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
+
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns String[]. Parameters must match
+     * those of the method.
+     * </p>
+     * 
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static Function<Object,String[]> methodForArrayOfString(final String methodName, final Object... optionalParameters) {
         return new Call<Object,String[]>(Types.ARRAY_OF_STRING, methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+
+    /**
+     * <p>
+     * Executes a method on the target object which returns List&lt;R&gt;, being <tt>R</tt> the
+     * specified type parameter. Parameters must match those of the method.
+     * </p>
+     * 
+     * @param resultType the type of the method's result
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,List<R>> methodForListOf(final Type<R> resultType, final String methodName, final Object... optionalParameters) {
         return new Call<Object,List<R>>(Types.listOf(resultType), methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
+
     
+    /**
+     * <p>
+     * Executes a method on the target object which returns Set&lt;R&gt;, being <tt>R</tt> the
+     * specified type parameter. Parameters must match those of the method.
+     * </p>
+     * 
+     * @param resultType the type of the method's result
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,Set<R>> methodForSetOf(final Type<R> resultType, final String methodName, final Object... optionalParameters) {
         return new Call<Object,Set<R>>(Types.setOf(resultType), methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
     
+    
+    /**
+     * <p>
+     * Executes a method on the target object which returns R[], being <tt>R</tt> the
+     * specified type parameter. Parameters must match those of the method.
+     * </p>
+     * 
+     * @param resultType the type of the method's result
+     * @param methodName the name of the method
+     * @param optionalParameters the (optional) parameters of the method. 
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,R[]> methodForArrayOf(final Type<R> resultType, final String methodName, final Object... optionalParameters) {
         return new Call<Object,R[]>(Types.arrayOf(resultType), methodName, VarArgsUtil.asOptionalObjectArray(Object.class,optionalParameters));
     }
