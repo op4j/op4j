8f1d62899c81207c6ddf1de83994bd11ae6b9725
Daniel Fernández <daniel.fernandez@11thlabs.org>
2010-04-15 22:53:16 +0000

    Several changes made
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@999 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/functions/Get.java
+++ src/main/java/org/op4j/functions/Get.java
@@ -35,6 +35,9 @@ import org.javaruntype.type.Types;
 
 
 /**
+ * <p>
+ * Function hub class for executing attribute getters in target objects.
+ * </p>
  * 
  * @since 1.0
  * 
@@ -49,88 +52,332 @@ public final class Get<T,R> extends Function<T,R> {
     
     
     
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Object.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Object> attrOfObject(final String attributeName) {
         return new Get<Object,Object>(Types.OBJECT, attributeName);
     }
+
     
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns R, being
+     * <tt>R</tt> the specified type.
+     * </p>
+     * <p>
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     *
+     * @param resultType the type of the attribute
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,R> attrOf(final Type<R> resultType, final String attributeName) {
         return new Get<Object,R>(resultType, attributeName);
     }
 
 
     
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns BigInteger.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,BigInteger> attrOfBigInteger(final String attributeName) {
         return new Get<Object,BigInteger>(Types.BIG_INTEGER, attributeName);
     }
     
+
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns BigDecimal.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,BigDecimal> attrOfBigDecimal(final String attributeName) {
         return new Get<Object,BigDecimal>(Types.BIG_DECIMAL, attributeName);
     }
     
+
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Boolean.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Boolean> attrOfBoolean(final String attributeName) {
         return new Get<Object,Boolean>(Types.BOOLEAN, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Byte.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Byte> attrOfByte(final String attributeName) {
         return new Get<Object,Byte>(Types.BYTE, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Character.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Character> attrOfCharacter(final String attributeName) {
         return new Get<Object,Character>(Types.CHARACTER, attributeName);
     }
     
+
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Calendar.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Calendar> attrOfCalendar(final String attributeName) {
         return new Get<Object,Calendar>(Types.CALENDAR, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Date.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Date> attrOfDate(final String attributeName) {
         return new Get<Object,Date>(Types.DATE, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Double.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Double> attrOfDouble(final String attributeName) {
         return new Get<Object,Double>(Types.DOUBLE, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Float.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Float> attrOfFloat(final String attributeName) {
         return new Get<Object,Float>(Types.FLOAT, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Integer.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Integer> attrOfInteger(final String attributeName) {
         return new Get<Object,Integer>(Types.INTEGER, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Long.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Long> attrOfLong(final String attributeName) {
         return new Get<Object,Long>(Types.LONG, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Short.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Short> attrOfShort(final String attributeName) {
         return new Get<Object,Short>(Types.SHORT, attributeName);
     }
     
+    
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns String.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,String> attrOfString(final String attributeName) {
         return new Get<Object,String>(Types.STRING, attributeName);
     }
     
+
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns List&lt;String&gt;.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,List<String>> attrOfListOfString(final String attributeName) {
         return new Get<Object,List<String>>(Types.LIST_OF_STRING, attributeName);
     }
+
     
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Set&lt;String&gt;.
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,Set<String>> attrOfSetOfString(final String attributeName) {
         return new Get<Object,Set<String>>(Types.SET_OF_STRING, attributeName);
     }
+
     
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns String[].
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     * 
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static Function<Object,String[]> attrOfArrayOfString(final String attributeName) {
         return new Get<Object,String[]>(Types.ARRAY_OF_STRING, attributeName);
     }
     
+
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns List&lt;R&gt;,
+     * being <tt>R</tt> the specified type.
+     * </p>
+     * <p>
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     *
+     * @param resultType the component type of the result list type.
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,List<R>> attrOfListOf(final Type<R> resultType, final String attributeName) {
         return new Get<Object,List<R>>(Types.listOf(resultType), attributeName);
     }
     
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns Set&lt;R&gt;,
+     * being <tt>R</tt> the specified type.
+     * </p>
+     * <p>
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     *
+     * @param resultType the component type of the result set type.
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,Set<R>> attrOfSetOf(final Type<R> resultType, final String attributeName) {
         return new Get<Object,Set<R>>(Types.setOf(resultType), attributeName);
     }
     
+
+    /**
+     * <p>
+     * Executes a getter (<tt>getX()</tt>) on the target object which returns R[],
+     * being <tt>R</tt> the specified type.
+     * </p>
+     * <p>
+     * If the specified attribute is, for example, "<tt>name</tt>", the called method
+     * will be "<tt>getName()</tt>".
+     * </p>
+     *
+     * @param resultType the component type of the result array type.
+     * @param attributeName the name of the attribute
+     * @return the result of the method execution
+     */
     public static <R> Function<Object,R[]> attrOfArrayOf(final Type<R> resultType, final String attributeName) {
         return new Get<Object,R[]>(Types.arrayOf(resultType), attributeName);
     }
