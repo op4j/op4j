9610e2e8a7204f0fe1669202d77deeffa3c9840e
Soraya Sánchez <soraya.schz@gmail.com>
2010-11-19 17:32:00 +0000

    Minor changes
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j@1175 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/functions/FnInteger.java
+++ src/main/java/org/op4j/functions/FnInteger.java
@@ -2871,7 +2871,7 @@ public final class FnInteger {
      * <p>
      * Determines whether the target object is between min and max
      * in value, this is, whether 
-     * <tt>target.compareTo(min) &gt;= 0 && target.compareTo(max) &lt;= 0</tt>. 
+     * <tt>target.compareTo(min) &gt;= 0 &amp;&amp; target.compareTo(max) &lt;= 0</tt>. 
      * The target and the specified min and max have to implement {@link Comparable}.
      * </p>
      * 
@@ -2887,7 +2887,7 @@ public final class FnInteger {
      * <p>
      * Determines whether the target object is between min and max
      * in value, this is, whether 
-     * <tt>target.compareTo(min) &gt;= 0 && target.compareTo(max) &lt;= 0</tt>. 
+     * <tt>target.compareTo(min) &gt;= 0 &amp;&amp; target.compareTo(max) &lt;= 0</tt>. 
      * The target and the specified min and max have to implement {@link Comparable}.
      * </p>
      * 
